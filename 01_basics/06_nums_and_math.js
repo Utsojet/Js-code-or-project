663bc4f5c27821392c94f171b0620c2358bf41f4
Utsojet <125271400+Utsojet@users.noreply.github.com>
2024-11-14 01:38:59 +0000

    number and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,32 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds =10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++Math+++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4)); //absolute value
+// console.log(Math.round(4.6)); //round figure
+// console.log(Math.ceil(4.2)); //celing value
+// console.log(Math.floor(4.9)); //floor value
+// console.log(Math.min(4, 3, 6, 8)); minimum value
+// console.log(Math.max(4, 3, 6, 8)); maximum value
+
+console.log(Math.random());
+console.log(Math.random()*10 +1);
+
+const min=10
+const max = 20
+
+console.log(Math.floor(Math.random() *(max-min+1)+min));
